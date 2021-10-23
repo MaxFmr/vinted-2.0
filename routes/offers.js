84a91134bb4fca14d013c2af4504d3da1ee4fea7
Maxime Kerlidou <maximekerlidou@MacBook-Air-de-Maxime.local>
2021-10-23 15:57:53 +0200

    update

--- routes/offers.js
+++ routes/offers.js
@@ -75,109 +75,79 @@ router.post("/user/offer/publish", isAuthenticated, async (req, res) => {
 
 router.get("/offers", async (req, res) => {
   try {
-    if (req.query.page) {
-      const numberOfProduct = 3;
-      const ObjectsToskip = Number(req.query.page) * numberOfProduct;
-      const offers = await Offer.find()
-        .limit(numberOfProduct)
-        .skip(ObjectsToskip);
-      res.json(offers);
+    const filters = {};
+
+    if (req.query.title) {
+      filters.product_name = new RegExp(req.query.title, "i");
     }
 
-    const offers = await Offer.find(
-      { product_name: new RegExp(req.query.title, "i") },
-      { product_price: { $lte: Number(req.query.priceMax) } },
-      { product_price: { $gte: Number(req.query.priceMin) } }
-    );
+    if (req.query.priceMin) {
+      filters.product_price = { $gte: req.query.priceMin };
+    }
 
-    res.json(product);
+    if (req.query.priceMax) {
+      if (filters.product_price) {
+        filters.product_price.$lte = req.query.priceMax;
+      } else {
+        filters.product_price = {
+          $lte: req.query.priceMax,
+        };
+      }
+    }
+
+    //console.log(filters);
+    let sort = {};
+
+    if (req.query.sort === "price-desc") {
+      sort = { product_price: -1 };
+    } else {
+      sort = { product_price: 1 };
+    }
+
+    // Par défaut on envoie la page 1
+    let page = 1;
+    if (req.query.page) {
+      page = Number(req.query.page);
+    }
+
+    // Par défaut on fixe la limite à 3
+    let limit = 3;
+    if (req.query.limit) {
+      limit = Number(req.query.limit);
+    }
+
+    const offers = await Offer.find(filters)
+      .sort(sort)
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .select("product_name product_price");
+
+    const count = await Offer.countDocuments(filters);
+
+    res.json({
+      count: count,
+      offers: offers,
+    });
   } catch (error) {
-    res.status(400).json({ error: { message: error.message } });
+    res.status(400).json({ message: error.message });
   }
-  
 });
 
-
-
-// router.get("/offers", async (req, res) => {
-//   try {
-//     const filters = {};
-
-//     if (req.query.title) {
-//       filters.product_name = new RegExp(req.query.title, "i");
-//     }
-
-//     if (req.query.priceMin) {
-//       filters.product_price = { $gte: req.query.priceMin };
-//     }
-
-//     if (req.query.priceMax) {
-//       if (filters.product_price) {
-//         filters.product_price.$lte = req.query.priceMax;
-//       } else {
-//         filters.product_price = {
-//           $lte: req.query.priceMax,
-//         };
-//       }
-//     }
-
-//     //console.log(filters);
-//     let sort = {};
-
-//     if (req.query.sort === "price-desc") {
-//       sort = { product_price: -1 };
-//     } else {
-//       sort = { product_price: 1 };
-//     }
-
-//     // Par défaut on envoie la page 1
-//     let page = 1;
-//     if (req.query.page) {
-//       page = Number(req.query.page);
-//     }
-
-//     // Par défaut on fixe la limite à 3
-//     let limit = 3;
-//     if (req.query.limit) {
-//       limit = Number(req.query.limit);
-//     }
-
-//     const offers = await Offer.find(filters)
-//       .sort(sort)
-//       .skip((page - 1) * limit)
-//       .limit(limit)
-//       .select("product_name product_price");
-
-//     const count = await Offer.countDocuments(filters);
-
-//     res.json({
-//       count: count,
-//       offers: offers,
-//     });
-//   } catch (error) {
-//     res.status(400).json({ message: error.message });
-//   }
-// });
-
-// router.get("/offer/:id", async (req, res) => {
-//   try {
-//     const id = req.params.id;
-//     const offer = await Offer.findById(id).populate({
-//       path: "owner",
-//       select: "account",
-//     });
-//     if (offer) {
-//       res.json(offer);
-//     } else {
-//       res.status(400).json({ message: "Offer not found" });
-//     }
-//   } catch (error) {
-//     res.status(400).json({ message: error.message });
-//   }
-// });
+router.get("/offer/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const offer = await Offer.findById(id).populate({
+      path: "owner",
+      select: "account",
+    });
+    if (offer) {
+      res.json(offer);
+    } else {
+      res.status(400).json({ message: "Offer not found" });
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
 module.exports = router;
-
-
-
-
